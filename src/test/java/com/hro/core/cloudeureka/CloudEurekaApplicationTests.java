cc7bec29ce62b48a3b1c9b6e743610a14fd994f9
hero <361754100@qq.com>
2020-02-07 15:40:04 +0800

    Initial commit

---
+++ src/test/java/com/hro/core/cloudeureka/CloudEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hro.core.cloudeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
